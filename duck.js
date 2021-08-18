ba93a87579758c9b19a870760c8dee3066ab5976
artesient <icicle.bear212@gmail.com>
2021-08-18 11:55:09 -0700

    changed these
    deleted unnecessary lines of code

--- duck.js
+++ duck.js
@@ -1,4 +1,8 @@
-var ctx = myCanvas.getContext("2d");
+var ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(myCanvas.width/2, myCanvas.length/2, 50, 50);
+var duck = new Image();
+duck.src = "https://artesient.github.io/ducksprite.png";
+
+image.onload = function() {
+    ctx.drawImage(duck, 0, 0);
+}
