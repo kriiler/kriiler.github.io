7473a3895c34684d57ddae637527ed27564dbac9
artesient <icicle.bear212@gmail.com>
2021-11-28 19:41:32 -0800

    Delete duck.js

--- duck.js
+++
@@ -1,8 +0,0 @@
-var ctx = canvas.getContext("2d");
-
-var duck = new Image();
-duck.src = "https://artesient.github.io/ducksprite.png";
-
-image.onload = function() {
-    ctx.drawImage(duck, 0, 0);
-}
